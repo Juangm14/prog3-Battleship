cbd2876f5acbc08c3016ce6d61873a16d4bd3f02
johns <johns@LAPTOP-O2K0IQJ9>
2020-10-05 13:36:48 +0200

    Practica 2

--- prog3-battleship/src/model/Ship.java
+++ prog3-battleship/src/model/Ship.java
@@ -2,5 +2,28 @@ package model;
 
 public class Ship {
 	
-
+	private int BOUNDING_SQUARE_SIZE;
+	private int HIT_VALUE;
+	private int CRAFT_VALUE;
+	private char symbol;
+	private String name;	
+	private int shape[][];
+	
+	public Ship(Orientation o, char s, String n) {
+		
+	}
+	
+	public Coordinate getPosition() {
+		
+	}
+	
+	public void setPosition(Coordinate position) {
+		
+	}
+	
+	public String getName() {
+		
+	}
+	
+	
 }
