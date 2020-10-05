a3c54205ef69a1b44117ec04d9bbe7f658959c2f
johns <johns@LAPTOP-O2K0IQJ9>
2020-10-05 13:56:44 +0200

    Practica 2

--- prog3-battleship/src/model/Board.java
+++ prog3-battleship/src/model/Board.java
@@ -2,4 +2,27 @@ package model;
 
 public class Board {
 
+	public char HIT_SYMBOL;
+	public char WATER_SYMBOL;
+	public char NOTSEEN_SYMBOL;
+	public int MAX_BOARD_SIZE;
+	public int MIN_BOARD_SIZE;
+	
+	private int size;
+	private int numCrafts;
+	private int destroyedCrafts;
+	
+	public Board(int size) {
+		
+	}
+	
+	public int getSize() {
+		
+	}
+	
+	public boolean checkCoordinate(Coordinate c) {
+		
+	}
+	
+	
 }
