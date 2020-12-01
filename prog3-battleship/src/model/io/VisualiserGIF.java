9696b588c016ca0860a4823b664a96d6c2a20d1d
johns <johns@DESKTOP-4LH5BK3>
2020-12-01 23:41:03 +0100

    P4

--- prog3-battleship/src/model/io/VisualiserGIF.java
+++ prog3-battleship/src/model/io/VisualiserGIF.java
@@ -1,31 +1,41 @@
 package model.io;
 
+import model.Board;
+import model.exceptions.io.BattleshipIOException;
+
+import java.awt.Color;
+import java.io.File;
 import model.io.gif.AnimatedGIF;
 import model.io.gif.FrameGIF;
 
 public class VisualiserGIF {
 
 	private Game game;
+	AnimatedGIF animatedG;
 	
 	VisualiserGIF(Game game) throws NullPointerException{
 		if(game != null) {
 			this.game = game;
-			AnimatedGIF animatedG;
+			
 		}else {
 			throw new NullPointerException();
 		}
 	}
 	
 	public void show() {
-		this.game.getBoard1().show(false);
-		this.game.getBoard2().show(false);
+		String printTablero1 = this.game.getBoard1().show(false);
+		String printtablero2 = this.game.getBoard2().show(false);
+		
 		
 		FrameGIF frame1 = new FrameGIF(this.game.getBoard1().getSize(), this.game.getBoard1().getSize()*2+1);
-		FrameGIF frame2 = new FrameGIF(this.game.getBoard1().getSize(), this.game.getBoard1().getSize()*2+1);
+		FrameGIF frame2 = new FrameGIF(this.game.getBoard2().getSize(), this.game.getBoard2().getSize()*2+1);
+		
 		
 	}
 	
-	public void close() {
+	public void close() throws BattleshipIOException {
 		
+		
+		animatedG.saveFile(new File("files/output.gif"));
 	}
 }
